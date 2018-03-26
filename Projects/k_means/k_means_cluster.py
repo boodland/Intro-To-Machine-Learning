0c33399fba90f329aaf8e370e6e33da4e6055f7e
Alberto Arias <alberto@boodland.com>
2018-03-26 12:04:34 +0200

    Add k-means quiz 20 solution

--- Projects/k_means/k_means_cluster.py
+++ Projects/k_means/k_means_cluster.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python 
-
 """ 
     Skeleton code for k-means clustering mini-project.
 """
@@ -30,7 +28,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     if mark_poi:
         for ii, pp in enumerate(pred):
             if poi[ii]:
-                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
+                plt.scatter(features[ii][0], features[ii][1], c="red", marker="*")
     plt.xlabel(f1_name)
     plt.ylabel(f2_name)
     plt.savefig(name)
@@ -39,7 +37,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -59,18 +57,20 @@ poi, finance_features = targetFeatureSplit( data )
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+    plt.scatter( f1, f2, c="blue" )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2, random_state=0)
+kmeans.fit(data)
+pred= kmeans.predict(data)
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=True, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print("no predictions object named pred found, no clusters to plot")
