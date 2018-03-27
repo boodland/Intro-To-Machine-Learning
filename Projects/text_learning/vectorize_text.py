2bc85db6fb99ef5f901d2485137617f42d2b5d9a
Alberto Arias <alberto@boodland.com>
2018-03-27 14:00:51 +0200

    Add text learning quiz 19 solution

--- Projects/text_learning/vectorize_text.py
+++ Projects/text_learning/vectorize_text.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 
 import os
 import pickle
@@ -34,37 +33,39 @@ word_data = []
 ### can take a long time
 ### temp_counter helps you only look at the first 200 emails in the list so you
 ### can iterate your modifications quicker
-temp_counter = 0
+# temp_counter = 0
 
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        # temp_counter += 1
+        # if temp_counter < 200:
+        path = os.path.join('..', path[:-1])
+        print(path)
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        ### use parseOutText to extract the text from the opened email
+        email_parsed = parseOutText(email)
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        import re
+        email_parsed_replaced = re.sub(r'sara|shackleton|chris|germani', r'', email_parsed)
+        ### append the text to word_data
+        word_data.append(email_parsed_replaced)
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        from_data.append(int(name == "chris"))
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        email.close()
 
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
-
-print "emails processed"
+print("emails processed")
 from_sara.close()
 from_chris.close()
+print(word_data[152])
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
 
