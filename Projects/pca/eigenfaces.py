5b2adc392b28d3797ac8069e3109f1e5416d932e
Alberto Arias <alberto@boodland.com>
2018-04-03 13:47:53 +0200

    Add pca quiz 34 solution

--- Projects/pca/eigenfaces.py
+++ Projects/pca/eigenfaces.py
@@ -74,7 +74,7 @@ pca = PCA(n_components=n_components, whiten=True, svd_solver='randomized').fit(X
 print("done in %0.3fs" % (time() - t0))
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
-
+print(pca.explained_variance_ratio_)
 print("Projecting the input data on the eigenfaces orthonormal basis")
 t0 = time()
 X_train_pca = pca.transform(X_train)
