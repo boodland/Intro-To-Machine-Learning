c8ab677f4f84be199db5fb064449334a68357c34
Alberto Arias <alberto@boodland.com>
2018-04-04 13:09:28 +0200

    Add validation quiz 17 solution and dependencies

---
+++ Projects/validation/validate_poi.py
@@ -0,0 +1,41 @@
+
+"""
+    Starter code for the validation mini-project.
+    The first step toward building your POI identifier!
+
+    Start by loading/formatting the data
+
+    After that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
+
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
+labels, features = targetFeatureSplit(data)
+
+
+
+### it's all yours from here forward!  
+from time import time
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier()
+t0 = time()
+clf.fit(features, labels)
+print("training time:", round(time()-t0, 3), "s")
+t1 = time()
+pred = clf.predict(features)
+print("prediction time:", round(time()-t1, 3), "s")
+accuracy = accuracy_score(pred, labels)
+print(accuracy)
